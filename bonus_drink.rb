a1d4b02c9037f0606154800de27d7f0252609313
chocolate-kuma <natsumi0814okada@gmail.com>
2023-10-23 02:14:36 +0900

    answer_6

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,9 +1,11 @@
-def total_count_for(initial_bottles)
-  total_bottles = initial_bottles
-  while initial_bottles >= 3
-    free_bottles = initial_bottles / 3
-    total_bottles += free_bottles.to_i
-    initial_bottles = free_bottles.to_i + (initial_bottles % 3)
+class BonusDrink
+  def total_count_for(initial_bottles)
+    total_bottles = initial_bottles
+    while initial_bottles >= 3
+      free_bottles = initial_bottles / 3
+      total_bottles += free_bottles.to_i
+      initial_bottles = free_bottles.to_i + (initial_bottles % 3)
+    end
+    total_bottles
   end
-  total_bottles
 end
