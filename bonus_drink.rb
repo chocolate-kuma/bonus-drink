08daa52300552f9e9142a7c9d5ce780a35f24443
chocolate-kuma <natsumi0814okada@gmail.com>
2023-10-23 02:20:07 +0900

    add self.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,5 @@
 class BonusDrink
-  def total_count_for(initial_bottles)
+  def self.total_count_for(initial_bottles)
     total_bottles = initial_bottles
     while initial_bottles >= 3
       free_bottles = initial_bottles / 3
